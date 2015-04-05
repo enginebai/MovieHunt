5c028ad321c895f6d32446afdaba6b3266dddb54
enginebai <ericwei.c63amg@gmail.com>
2015-04-05 11:36:06 +0800

    Import FloatingActionButton and add it to movie list and detail layout (#10, #12)

--- app/src/main/java/com/moviebomber/ui/activity/MovieDetailActivity.java
+++ app/src/main/java/com/moviebomber/ui/activity/MovieDetailActivity.java
@@ -43,9 +43,6 @@ public class MovieDetailActivity extends ActionBarActivity {
 		int id = item.getItemId();
 
 		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
 
 		return super.onOptionsItemSelected(item);
 	}
