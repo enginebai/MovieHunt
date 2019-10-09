2c51da92f6d5224f80f8a5c5e284c93f3e0a7ea7
Engine Bai <enginebai@gmail.com>
2019-10-09 17:06:59 +0800

    Create base project; integrate gradle buildSrc successfully

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,4 @@
+
+object Dependencies {
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,4 @@
+
+object Versions {
+    const val kotlin = "1.3.50"
+}
