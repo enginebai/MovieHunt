5780bce02b36148f03bfbc98c17408986ea629cd
Engine Bai <enginebai@gmail.com>
2019-10-16 19:09:59 +0800

    Migrate project, module build.gradle and settings.gradle to kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = "1.3.50"
     repositories {
         google()
         jcenter()
         
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:3.5.1"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.5.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -23,6 +22,8 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
 }
