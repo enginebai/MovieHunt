c27553dbd897c428222ab328352c0329953f5233
Engine Bai <enginebai@gmail.com>
2019-10-25 17:49:16 +0800

    Add common dependencies kts file and apply it

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,9 +5,10 @@ plugins {
     id("config")
 }
 
+apply(from = "../dependencies.gradle.kts")
+
 dependencies {
     implementation(fileTree("dir" to "libs", "include" to listOf("*.jar")))
-    implementation(Dependencies.Kotlin.stdLib)
     implementation(Dependencies.AndroidX.appCompat)
     implementation(Dependencies.AndroidX.coreKtx)
     implementation(Dependencies.AndroidX.constraintLayout)
@@ -15,15 +16,9 @@ dependencies {
     androidTestImplementation(Dependencies.Test.runner)
     androidTestImplementation(Dependencies.Test.espressoCore)
 
-    implementation(Dependencies.rxJava)
-    implementation(Dependencies.rxAndroid)
-
     implementation(Dependencies.okhttp)
     implementation(Dependencies.okhttpLogging)
     implementation(Dependencies.Retrofit.core)
     implementation(Dependencies.Retrofit.gsonConverter)
     implementation(Dependencies.Retrofit.rxJavaAdapter)
-
-    implementation(Dependencies.Koin.android)
-    implementation(Dependencies.Koin.viewModel)
 }
---
+++ dependencies.gradle.kts
@@ -0,0 +1,11 @@
+val implementation by configurations
+
+dependencies {
+    implementation(Dependencies.Kotlin.stdLib)
+    
+    implementation(Dependencies.rxJava)
+    implementation(Dependencies.rxAndroid)
+
+    implementation(Dependencies.Koin.android)
+    implementation(Dependencies.Koin.viewModel)
+}
