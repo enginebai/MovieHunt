8a3b734a812cde7f5771281f9ee9d981bf56ceb6
Engine Bai <enginebai@gmail.com>
2020-03-11 10:09:48 +0800

    Fix Timber and Logger integration problem

--- base/src/main/java/com/enginebai/base/di/UtilsModule.kt
+++ base/src/main/java/com/enginebai/base/di/UtilsModule.kt
@@ -1,20 +1,34 @@
 package com.enginebai.base.di
 
 import com.enginebai.base.BuildConfig
-import com.enginebai.base.utils.logging.MyCustomLoggingDebugTree
+import com.enginebai.base.utils.logging.TimberLoggerDebugTree
+import com.google.gson.Gson
 import com.orhanobut.logger.AndroidLogAdapter
-import com.orhanobut.logger.LogAdapter
+import com.orhanobut.logger.FormatStrategy
+import com.orhanobut.logger.PrettyFormatStrategy
 import org.koin.dsl.module
 import timber.log.Timber
 
 val loggingModule = module {
-    single<LogAdapter> {
-        object : AndroidLogAdapter() {
+    single<AndroidLogAdapter> { (formatStrategy: FormatStrategy) ->
+        object : AndroidLogAdapter(formatStrategy) {
             override fun isLoggable(priority: Int, tag: String?): Boolean {
                 return BuildConfig.DEBUG
             }
         }
     }
 
-    single<Timber.Tree> { MyCustomLoggingDebugTree() }
+    single<FormatStrategy> {
+        PrettyFormatStrategy.newBuilder()
+            .tag("enginebai")
+            .methodCount(3)
+            .methodOffset(5) // avoid timber internal stack track
+            .build()
+    }
+
+    single<Timber.Tree> { TimberLoggerDebugTree() }
+}
+
+val gsonModule = module {
+    single { Gson() }
 }
