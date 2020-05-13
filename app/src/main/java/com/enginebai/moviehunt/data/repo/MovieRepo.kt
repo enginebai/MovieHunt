c8815b6a3e24297062434a6e283622d6170cd356
enginebai <enginebai@gmail.com>
2020-05-13 22:20:17 +0800

    Add PagedList Listing data class for UI to observe; change MovieRepo.fetchMovieList() methdo to return Listing

--- app/src/main/java/com/enginebai/moviehunt/data/repo/MovieRepo.kt
+++ app/src/main/java/com/enginebai/moviehunt/data/repo/MovieRepo.kt
@@ -2,6 +2,7 @@ package com.enginebai.moviehunt.data.repo
 
 import androidx.paging.PagedList
 import androidx.paging.RxPagedListBuilder
+import com.enginebai.base.utils.Listing
 import com.enginebai.moviehunt.data.local.MovieModel
 import com.enginebai.moviehunt.data.remote.MovieApiService
 import com.enginebai.moviehunt.data.remote.MovieListDataSourceFactory
@@ -14,7 +15,7 @@ import org.koin.core.inject
 const val DEFAULT_PAGE_SIZE = 10
 
 interface MovieRepo {
-    fun fetchMovieList(category: MovieCategory, pageSize: Int = DEFAULT_PAGE_SIZE): Observable<PagedList<MovieModel>>
+    fun fetchMovieList(category: MovieCategory, pageSize: Int = DEFAULT_PAGE_SIZE): Listing<MovieModel>
 }
 
 class MovieRepoImpl : MovieRepo, KoinComponent {
@@ -22,14 +23,20 @@ class MovieRepoImpl : MovieRepo, KoinComponent {
     override fun fetchMovieList(
         category: MovieCategory,
         pageSize: Int
-    ): Observable<PagedList<MovieModel>> {
+    ): Listing<MovieModel> {
         val dataSourceFactory = MovieListDataSourceFactory(category)
         val pagedListConfig = PagedList.Config.Builder()
             .setPageSize(DEFAULT_PAGE_SIZE)
             .setEnablePlaceholders(false)
             .build()
-        return RxPagedListBuilder(dataSourceFactory, pagedListConfig)
+        val pagedList = RxPagedListBuilder(dataSourceFactory, pagedListConfig)
             .setFetchScheduler(Schedulers.io())
             .buildObservable()
+	    return Listing(
+		    pagedList = pagedList,
+		    refreshState = dataSourceFactory.initLoadState,
+		    loadMoreState = dataSourceFactory.loadMoreState,
+		    refresh = { dataSourceFactory.dataSource?.invalidate() }
+	    )
     }
 }
