202aa556f686b40132d21b6e1868f0e6aeac3157
Engine Bai <enginebai@gmail.com>
2021-11-13 20:48:42 +0800

    Add color constants

---
+++ app/src/main/java/com/enginebai/moviehunt/resources/Colors.kt
@@ -0,0 +1,20 @@
+package com.enginebai.moviehunt.resources
+
+import androidx.compose.ui.graphics.Color
+
+object ColorsPalette {
+    val colorPrimary = Color(0xFF121e2d)
+    val colorPrimaryDark = Color(0xFF121e2d)
+    val colorAccent = Color(0xFF3bdeee)
+
+    val darkBlue = Color(0xFF17293F)
+    val yellow = Color(0xFFFFC42E)
+    val grey = Color(0xFFA7A7A9)
+}
+
+object MyColors {
+    val placeholder = Color(0xFF293E58)
+    val ratingBackground = Color(0xFF293E58)
+    val background = ColorsPalette.colorPrimary
+    val cardBackground = ColorsPalette.darkBlue
+}
